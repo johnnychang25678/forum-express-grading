2b29da7bb4bea2afa63caba7df603c2f6c6565c1
Johnny Chang <zjps3407@gmail.com>
2021-01-29 14:38:41 +0800

    register confirm password and unique email

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const restController = require('../controllers/restController')
 const adminController = require('../controllers/adminController')
+const userController = require('../controllers/userController')
 
 module.exports = (app) => {
   app.get('/', (req, res) => res.redirect('/restaurants'))
   app.get('/restaurants', restController.getRestaurants)
   app.get('/admin', (req, res) => res.redirect('/admin/restaurants'))
   app.get('/admin/restaurants', adminController.getRestaurants)
+  app.get('/signup', userController.signUpPage)
+  app.post('/signup', userController.signUp)
 }
