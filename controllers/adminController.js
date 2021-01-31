c1e52aeef146b9cabe5422d43122300386006695
Johnny Chang <zjps3407@gmail.com>
2021-01-31 14:12:46 +0800

    add admin update restaurant feature

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -2,24 +2,27 @@ const db = require('../models')
 const Restaurant = db.Restaurant
 
 const adminController = {
+  // show all restaurants
   getRestaurants: (req, res) => {
     Restaurant.findAll({
       raw: true
     }).then(restaurants => res.render('admin/restaurants', { restaurants }))
   },
+  // show single restaurant
   getRestaurant: (req, res) => {
     return Restaurant.findByPk(req.params.id, { raw: true })
       .then(restaurant => {
         return res.render('admin/restaurant', { restaurant })
       })
   },
+  // create form
   createRestaurants: (req, res) => {
     return res.render('admin/create')
   },
+  // create new restaurant
   postRestaurant: (req, res) => {
     const { name, tel, address, opening_hours, description } = req.body
     if (!name) {
-      console.log('no name!')
       req.flash('error_messages', "name didn't exist")
       return res.redirect('back')
     }
@@ -30,6 +33,32 @@ const adminController = {
       res.redirect('/admin/restaurants')
     })
   },
+  // edit form
+  editRestaurant: (req, res) => {
+    return Restaurant.findByPk(req.params.id, { raw: true })
+      .then(restaurant => {
+        return res.render('admin/create', { restaurant })
+      })
+  },
+  // update restaurant info
+  putRestaurant: (req, res) => {
+    const { name, tel, address, opening_hours, description } = req.body
+    if (!name) {
+      req.flash('error_messages', "name didn't exist")
+      return res.redirect('back')
+    }
+    return Restaurant.findByPk(req.params.id) // no need raw:true since we still need to use sequelize update
+      .then(restaurant => {
+        return restaurant.update({ // sequelize update
+          name, tel, address, opening_hours, description
+        })
+      })
+      .then(restaurant => {
+        req.flash('success_messages', 'restaurant was successfully updated')
+        return res.redirect('/admin/restaurants')
+      })
+
+  }
 }
 
 module.exports = adminController
