26079a95578ff2a9fbb81f65dd1556bc767e8083
Johnny Chang <zjps3407@gmail.com>
2021-02-04 15:25:05 +0800

    create comment model

--- controllers/restController.js
+++ controllers/restController.js
@@ -2,18 +2,26 @@ const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
 
+const pageLimit = 10
+
 const restController = {
   getRestaurants: (req, res) => {
+    let offset = 0
     const whereQuery = {}
     let categoryId = ''
+    if (req.query.page) {
+      offset = (req.query.page - 1) * pageLimit
+    }
     if (req.query.categoryId) {
       categoryId = Number(req.query.categoryId) // convert form str to number for sequelize where query
       whereQuery.CategoryId = categoryId // {CategoryId: 1}
     }
 
-    const restaurantFindAll = Restaurant.findAll({
+    const restaurantFindAll = Restaurant.findAndCountAll({ // returns count: # of rows, row:{data}
       include: [Category],
-      where: whereQuery
+      where: whereQuery,
+      offset: offset,
+      limit: pageLimit
     })
     const categoryFindAll = Category.findAll({
       raw: true,
@@ -22,7 +30,13 @@ const restController = {
 
     Promise.all([restaurantFindAll, categoryFindAll])
       .then(([restaurants, categories]) => {
-        const data = restaurants.map(restaurant => {
+        const page = Number(req.query.page) || 1
+        const pages = Math.ceil(restaurants.count / pageLimit)
+        const totalPage = Array.from({ length: pages }).map((_, index) => index + 1)
+        const prev = page - 1 < 1 ? 1 : page - 1
+        const next = page + 1 > pages ? pages : page + 1
+
+        const data = restaurants.rows.map(restaurant => {
           return {
             ...restaurant.dataValues,
             description: restaurant.dataValues.description.substring(0, 50),
@@ -32,7 +46,11 @@ const restController = {
         return res.render('restaurants', {
           restaurants: data,
           categories,
-          categoryId
+          categoryId,
+          page,
+          totalPage,
+          prev,
+          next
         })
       })
 
