c6bdfbdca06da38beb1ac3f2e7f9e0992a319906
Johnny Chang <zjps3407@gmail.com>
2021-02-10 15:23:09 +0800

    add api route

---
+++ routes/apis.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+
+
+const adminController = require('../controllers/api/adminController.js')
+
+router.get('/admin/restaurants', adminController.getRestaurants)
+
+module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,8 @@
 const express = require('express')
 const router = express.Router()
-const { authenticatedAdmin, authenticatedUser } = require('../middlewares/authentications')
-// router modules
-const admin = require('./modules/admin')
-const home = require('./modules/home')
-const restaurant = require('./modules/restaurant')
-const user = require('./modules/user')
 
 
-router.use('/admin', authenticatedAdmin, admin)
-router.use('/restaurants', authenticatedUser, restaurant)
-router.use('/users', authenticatedUser, user) // for edit profile
-router.use('/', home)
-
-
+router.use('/', require('./routes'))
+router.use('/api', require('./apis'))
 
 module.exports = router
